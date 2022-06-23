ea274b84d017648ed1c893180f89fecd533e3c0b
Screamer <999screamer999@gmail.com>
2022-06-23 17:40:40 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,47 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {LoginComponent} from './login/login.component';
+import {AuthModule} from '@auth0/auth0-angular';
+import {AuthButtonComponent} from './auth-button/auth-button.component';
+import {MatToolbarModule} from '@angular/material/toolbar'
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {MatSliderModule} from '@angular/material/slider';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatMenuModule} from "@angular/material/menu";
+import {MatBadgeModule} from "@angular/material/badge";
+import {MatCardModule} from "@angular/material/card";
+import { ResumeComponent } from './resume/resume.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    AuthButtonComponent,
+    ResumeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AuthModule.forRoot({
+      domain: 'dev-rd1axuv2.us.auth0.com',
+      clientId: 'SkzVwd3mKMO2uOJCP7ocKv5I6aDCkpec'
+    }),
+    MatToolbarModule,
+    MatIconModule,
+    MatSliderModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatBadgeModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
