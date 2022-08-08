7fac845abb676c9aff9b491101ac3f220ded46ba
Screamer <999screamer999@gmail.com>
2022-08-08 16:23:20 +0300

    [fix] change github actions secrets call

--- auth_config.ts
+++ auth_config.ts
@@ -1,4 +1,4 @@
 export const AUTH_CONFIG = Object({
-  "domain": '${process.env.AUTH0_DOMAIN}',
-  "clientId": '${process.env.AUTH0_CLIENT_ID}'
+  "domain": ${process.env.AUTH0_DOMAIN},
+  "clientId": ${process.env.AUTH0_CLIENT_ID}
 })
