df3e6ba82613bc72f2ce8c65918cd6451688376e
Screamer <999screamer999@gmail.com>
2022-08-08 16:17:41 +0300

    [feature] change credentials to github actions secrets

--- auth_config.ts
+++ auth_config.ts
@@ -1,4 +1,4 @@
 export const AUTH_CONFIG = Object({
-  "domain": "dev-rd1axuv2.us.auth0.com",
-  "clientId": "SkzVwd3mKMO2uOJCP7ocKv5I6aDCkpec"
+  "domain": '${process.env.AUTH0_DOMAIN}',
+  "clientId": '${process.env.AUTH0_CLIENT_ID}'
 })
