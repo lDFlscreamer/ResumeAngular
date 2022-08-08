3c86818cc2a79e03f361ebe62f0a6854f2b39585
Screamer <999screamer999@gmail.com>
2022-08-08 15:14:45 +0300

    feat( environment.prod.ts) import env value from environment.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,6 @@
-import {AUTH_CONFIG} from "../../auth_config";
+import {environment as env} from "./environment";
 
 export const environment = {
-  production: true,
-  auth: {
-    domain:AUTH_CONFIG.domain,
-    clientId:AUTH_CONFIG.clientId,
-    redirectUri: window.location.origin
-  },
-  ID_TOKEN_NAMESPACE_PREFIX: 'https://resume.com'
+  ...env,
+  production: true
 };
