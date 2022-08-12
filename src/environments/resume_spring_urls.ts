377436fc0540c44819d929288c54edad2320d2ec
Screamer <999screamer999@gmail.com>
2022-08-12 13:47:03 +0300

    [refactor]  add api_url constant

--- src/environments/resume_spring_urls.ts
+++ src/environments/resume_spring_urls.ts
@@ -3,6 +3,8 @@ export const BACK_END_URL={
   SERVER_API_VERSION:1,
   API_PREFIX:'/api/v',
   ENDPOINTS:{
-    JAVA_CV_PDF_FILE:'/resource/CV/Java'
+    JAVA_CV_PDF_FILE:'/resource/CV/Java',
+    MESSAGE:'/message'
   }
 }
+export const API_URL = BACK_END_URL.SERVER_URL.concat(BACK_END_URL.API_PREFIX).concat(String(BACK_END_URL.SERVER_API_VERSION));
