6104ebd928955eb6816605074f6620d4f451ae5f
Screamer <999screamer999@gmail.com>
2022-08-12 13:47:59 +0300

    [refactor]  use api_url constant to get pdf cv

--- src/app/resume-components/resume-pdf-view/resume-pdf-view.component.ts
+++ src/app/resume-components/resume-pdf-view/resume-pdf-view.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {BACK_END_URL} from "../../../environments/resume_spring_urls"
+import {API_URL, BACK_END_URL} from "../../../environments/resume_spring_urls"
 
 @Component({
   selector: 'app-resume-pdfs-view',
@@ -15,10 +15,7 @@ export class ResumePdfViewComponent implements OnInit {
 
 
   getPDF_Src() {
-    // request or pass into variable back end CV pdf link
-    let apiUrl = BACK_END_URL.SERVER_URL.concat(BACK_END_URL.API_PREFIX).concat(String(BACK_END_URL.SERVER_API_VERSION));
-    let javaCvPdfUrl = apiUrl.concat(BACK_END_URL.ENDPOINTS.JAVA_CV_PDF_FILE);
-    return javaCvPdfUrl
+    return API_URL.concat(BACK_END_URL.ENDPOINTS.JAVA_CV_PDF_FILE)
   }
 
 }
